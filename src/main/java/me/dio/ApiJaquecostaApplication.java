27fd39436ec9d45080c8462d21b8277ad8d4f142
Jaqueline Costa <143290999+jaquecoxta@users.noreply.github.com>
2023-10-21 01:16:32 -0300

    Commit1

---
+++ src/main/java/me/dio/ApiJaquecostaApplication.java
@@ -0,0 +1,14 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJaquecostaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJaquecostaApplication.class, args);
+	}
+
+}
+
