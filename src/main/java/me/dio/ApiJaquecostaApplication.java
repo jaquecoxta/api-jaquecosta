c3aefe3cd88e00987c8a44dfb2c0dc69955dd3e3
Jaqueline Costa <143290999+jaquecoxta@users.noreply.github.com>
2023-10-21 12:06:34 -0300

    Update ApiJaquecostaApplication.java
    Adicionando código JSON

--- src/main/java/me/dio/ApiJaquecostaApplication.java
+++ src/main/java/me/dio/ApiJaquecostaApplication.java
@@ -8,7 +8,35 @@ public class ApiJaquecostaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiJaquecostaApplication.class, args);
+
+		// JSON fornecido
+		String json = "{" +
+				"\"name\": \"Jaqueline\"," +
+				"\"account\": {" +
+				"\"number\": \"00000-0\"," +
+				"\"agency\": \"0000\"," +
+				"\"balance\": 1324.64," +
+				"\"limit\": 1000.00" +
+				"}," +
+				"\"features\": [" +
+				"{" +
+				"\"icon\": \"URL\"," +
+				"\"description\": \"Descrição da Feature\"" +
+				"}" +
+				"]," +
+				"\"card\": {" +
+				"\"number\": \"xxxx xxxx xxxx 0000\"," +
+				"\"limit\": 1000.00" +
+				"}," +
+				"\"news\": [" +
+				"{" +
+				"\"icon\": \"URL\"," +
+				"\"description\": \"Descrição da Novidade\"" +
+				"}" +
+				"]" +
+				"}";
+
+		// Imprimir o JSON
+		System.out.println(json);
 	}
-
 }
-
